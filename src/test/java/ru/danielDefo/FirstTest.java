83fd19a63588be6d9c8a593bd9da81b3d814907e
SR71UAV <30587864+SR71UAV@users.noreply.github.com>
2019-03-12 10:21:16 +0300

    Initial commit

---
+++ src/test/java/ru/danielDefo/FirstTest.java
@@ -0,0 +1,32 @@
+package ru.danielDefo;
+
+import org.junit.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+    ChromeDriver driver;
+
+   @Before
+   public void before_test(){
+       System.setProperty("webdriver.chrome.driver", "/test/chromedriver.exe");
+       driver = new ChromeDriver();
+       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+       System.out.println("test started");
+   }
+
+    @After
+    public void after_test() {
+        driver.close();
+        System.out.println("test ended");
+    }
+
+
+    @Test
+    public void test_number_1() {
+        driver.get("https://danieldefo.ru");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("MALADEC IGORYAN"));
+    }
+
+}
